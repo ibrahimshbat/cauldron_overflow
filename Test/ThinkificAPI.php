1d68bef1db6a7edbc149683195d4b24fbb589f36
Ibrahim Elsanosi <ibrahimsabat@yahoo.com>
2021-06-22 11:54:57 +0100

    Make id value as UUid

--- Test/ThinkificAPI.php
+++ Test/ThinkificAPI.php
@@ -15,14 +15,15 @@ class ThinkificAPI
      */
     public function __construct()
     {
-        $this->client = new Client([
+       /* $this->client = new Client([
             'base_url' => 'https://api.thinkific.com/api/public/v1/',
             'headers'=>[
                 'X-Auth-API-Key' => '1c1d77854a35f4f97cd186c3431ba578',
                 'X-Auth-Subdomain' => SUBDOMAIN,
                 'Content-Type' => 'application/json',
                 ],
-            ]);
+            ]);*/
+       echo "Ibrahim". Uuid::uuid4()->toString();
     }
 
     public function getEnrolment(){
@@ -37,8 +38,10 @@ class ThinkificAPI
 
 
 }
+new ThinkificAPI();
 
-new ThinkificAPI().printtt();
+//echo Uuid::uuid4()->toString();
+//new ThinkificAPI().printtt();
 //Client $client = new Client(['base_uri' => 'https://evidencebased.education']);
 // Send a request to https://foo.com/api/test
 //$response = $client->request('GET', 'our-team');
